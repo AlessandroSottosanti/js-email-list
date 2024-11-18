e73d7478a81d055431075546abff65a84ce6c898
AlessandroSottosanti <asottosanti2@gmail.com>
2024-11-18 18:29:32 +0100

    feat: dinamic button

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ const $btnReload = $one('#reload'); // Ottengo il bottone per il ricaricamento d
  // Carica direttamente le prime 10 email
 getEmails(maxNum);
 
+
+// Carichiamo dinamicamente il numero di email da caricare nel testo del bottone
+$btnReload.innerText = `Carica altre ${maxNum} Email`;
+
 // Sostituisci le 10 email con altre al click
 
 $btnReload.addEventListener("click", function() {
